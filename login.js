0a6459516dbb0e7688f09e8624f72402fb0884d0
unknown <adiraaa81893@gmail.com>
2021-06-24 21:07:15 -0500

    success login page

---
+++ login.js
@@ -0,0 +1,16 @@
+
+
+function login(){
+    let u_name=uname.value;
+    let pwd=pswd.value;
+    let user=JSON.parse(localStorage.getItem(u_name))
+    if(user==null){
+        alert("invalid username")
+    }
+    if(user.username==u_name & user.password==pwd){
+        alert("login success")
+    }
+    else{
+        alert("invalid password")
+    }
+}
